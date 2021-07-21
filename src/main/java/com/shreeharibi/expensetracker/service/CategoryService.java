07c251058d71dd306a81ddaa0834ad4a5466ad0e
Shree Hari Bittugondanahalli Indra Kumar <shree_hari.bittugondanahalli_indra_kumar@mailbox.tu-dresden.de>
2021-07-21 21:02:05 +0200

    Add category service and controller
    Implement to API to get all available categories

---
+++ src/main/java/com/shreeharibi/expensetracker/service/CategoryService.java
@@ -0,0 +1,23 @@
+package com.shreeharibi.expensetracker.service;
+
+import com.shreeharibi.expensetracker.category.Category;
+import com.shreeharibi.expensetracker.category.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CategoryService {
+    private final CategoryRepository categoryRepository;
+
+    @Autowired
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    public List<Category> getCategories() {
+        return categoryRepository.findAll();
+    }
+}
