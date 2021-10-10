f50d952a1ce37bb567984b12fbda348241798b22
Shree Hari Bittugondanahalli Indra Kumar <shreehariuvce@gmail.com>
2021-10-10 22:31:55 +0200

    Disable SpringBootTest

--- src/test/java/com/shreeharibi/expensetracker/ExpenseTrackerApplicationTests.java
+++ src/test/java/com/shreeharibi/expensetracker/ExpenseTrackerApplicationTests.java
@@ -1,9 +1,11 @@
 package com.shreeharibi.expensetracker;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Disabled
 class ExpenseTrackerApplicationTests {
 
 	@Test
